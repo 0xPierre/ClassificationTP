2ec9207d499899ef722240b7812fc660e82d6396
adrien <a.d44@tuta.io>
2023-12-16 21:35:10 +0100

    feat(Decision): Add Decisions functions.

--- include/DecisionTree.h
+++ include/DecisionTree.h
@@ -14,4 +14,53 @@ typedef struct sDecisionTreeNode{
     int classID;
 } DecisionTreeNode;
 
+typedef struct sTreePurityRes{
+    int classID;
+    float threshold;
+} TreePurityRes;
+
 #endif //CLASSIFICATIONTP_DECISIONTREE_H
+
+/**
+ * Creates a decision tree node recursively based on the given subproblem and parameters.
+ *
+ * @param sp                The subproblem for which to create the decision tree node.
+ * @param currentDepth      The current depth of the node in the decision tree.
+ * @param maxDepth          The maximum depth allowed for the decision tree (stopping criterion).
+ * @param prunningThreshold The purity threshold for pruning the tree. If the purity of a subproblem
+ *                          exceeds this threshold, the node becomes a leaf node with the majority class.
+ * @return                  A pointer to the created decision tree node.
+ *                          If the current depth exceeds the maximum depth, NULL is returned (pruning).
+ */
+DecisionTreeNode* DecisionTree_create(
+        Subproblem* sp,
+        int currentDepth,
+        int maxDepth,
+        float prunningThreshold);
+
+/**
+ * Computes the purity of a subproblem by determining the dominant class and its relative frequency.
+ *
+ * @param sp    The subproblem for which to compute the purity.
+ * @return      A structure containing the index of the dominant class and its relative frequency.
+ *              The relative frequency is the ratio of the number of instances of the dominant class
+ *              to the total number of instances in the subproblem.
+ */
+TreePurityRes Tree_purity(Subproblem *sp);
+
+/**
+ * Recursively deallocates memory for a decision tree and its nodes.
+ *
+ * @param decisionTree   The root node of the decision tree to be destroyed.
+ *                      If NULL, no action is taken (base case of recursion).
+ */
+void DecisionTree_destroy(DecisionTreeNode *decisionTree);
+
+/**
+ * Counts the number of nodes in a decision tree, including the root node and its descendants.
+ *
+ * @param node  The root node of the decision tree for which to count the nodes.
+ *              If NULL, the count is zero (base case of recursion).
+ * @return      The total number of nodes in the decision tree.
+ */
+int Decision_nodeCount(DecisionTreeNode* node);
--- src/DecisionTree.c
+++ src/DecisionTree.c
@@ -3,3 +3,76 @@
 //
 
 #include "../include/DecisionTree.h"
+#include "stdlib.h"
+
+DecisionTreeNode *DecisionTree_create(
+        Subproblem *sp,
+        int currentDepth,
+        int maxDepth,
+        float prunningThreshold) {
+
+    if (currentDepth >= maxDepth) {
+        return NULL;
+    }
+
+    DecisionTreeNode *node = (DecisionTreeNode *) calloc(1, sizeof(DecisionTreeNode));
+
+    if (Tree_purity(sp).threshold >= prunningThreshold) {
+        node->classID = Tree_purity(sp).classID;
+        return node;
+    }
+
+    node->split = Split_compute(sp);
+
+    Subproblem *subProblemLeft = Subproblem_create(sp->instanceCount, sp->featureCount, sp->classCount);
+    Subproblem *subProblemRight = Subproblem_create(sp->instanceCount, sp->featureCount, sp->classCount);
+
+
+    for (int i = 0; i < sp->instanceCount; i++) {
+        Instance *instance = sp->instances[i];
+        if ((float) instance->values[node->split.featureID] < node->split.threshold) {
+            Subproblem_insert(subProblemLeft, instance);
+        } else {
+            Subproblem_insert(subProblemRight, instance);
+        }
+    }
+
+    node->left = DecisionTree_create(subProblemLeft, currentDepth + 1, maxDepth, prunningThreshold);
+    node->right = DecisionTree_create(subProblemRight, currentDepth + 1, maxDepth, prunningThreshold);
+
+    Subproblem_destroy(subProblemLeft);
+    Subproblem_destroy(subProblemRight);
+    return node;
+}
+
+TreePurityRes Tree_purity(Subproblem *sp) {
+    int maxInstanceCount = 0;
+    int index = 0;
+    for (int i = 0; i < sp->classCount; ++i) {
+        if (sp->classes[i].instanceCount > maxInstanceCount) {
+            maxInstanceCount = sp->classes[i].instanceCount;
+            index = i;
+        }
+    }
+
+    TreePurityRes res = {index, (float) maxInstanceCount / (float) sp->instanceCount};
+
+    return res;
+}
+
+
+void DecisionTree_destroy(DecisionTreeNode *decisionTree) {
+    if (decisionTree == NULL) {
+        return;
+    }
+    DecisionTree_destroy(decisionTree->left);
+    DecisionTree_destroy(decisionTree->right);
+    free(decisionTree);
+}
+
+int Decision_nodeCount(DecisionTreeNode* node) {
+    if (node == NULL) {
+        return 0;
+    }
+    return 1 + Decision_nodeCount(node->left) + Decision_nodeCount(node->right);
+}
