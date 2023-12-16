7027948f4897493d94d42edfbe12186256088798
adrien <a.d44@tuta.io>
2023-12-16 17:11:39 +0100

    feat(Split): Add Split_gini and Get_gini_score functions

--- include/Split.h
+++ include/Split.h
@@ -2,6 +2,8 @@
 // Created by adrien on 12/16/23.
 //
 
+#include "Dataset.h"
+
 #ifndef CLASSIFICATIONTP_SPLIT_H
 #define CLASSIFICATIONTP_SPLIT_H
 
@@ -11,3 +13,21 @@ typedef struct sSplit{
 } Split;
 
 #endif //CLASSIFICATIONTP_SPLIT_H
+
+/**
+ * Calculates the Gini impurity score after splitting a subproblem based on a specified feature and threshold.
+ *
+ * @param sp            The subproblem to be split.
+ * @param featureID     The ID of the feature used for splitting.
+ * @param threshold     The threshold value for the feature to determine the split.
+ * @return              The Gini impurity score after the split.
+ */
+float Split_gini(Subproblem *sp, int featureID, float threshold);
+
+/**
+ * Calculates the Gini impurity score for a given subproblem.
+ *
+ * @param sp    The subproblem for which to calculate the Gini impurity.
+ * @return      The Gini impurity score.
+ */
+float Get_gini_score(Subproblem * sp);
--- src/Split.c
+++ src/Split.c
@@ -3,3 +3,51 @@
 //
 
 #include "../include/Split.h"
+#include "math.h"
+
+float Split_gini(Subproblem *sp, int featureID, float threshold) {
+
+    Subproblem *subProblemLeft =
+            Subproblem_create(
+                    sp->instanceCount,
+                    sp->instanceCount,
+                    sp->classCount);
+    Subproblem *subProblemRight =
+            Subproblem_create(
+                    sp->instanceCount,
+                    sp->instanceCount,
+                    sp->classCount);
+
+    // Split instance to two sub problem, right and left
+    for (int i = 0; i < sp->instanceCount; ++i) {
+        if ((float) sp->instances[i]->values[featureID] < threshold) {
+            Subproblem_insert(subProblemLeft, sp->instances[i]);
+        } else {
+            Subproblem_insert(subProblemRight, sp->instances[i]);
+        }
+    }
+
+    float scoreLeft = Get_gini_score(subProblemLeft);
+    float scoreRight = Get_gini_score(subProblemRight);
+
+    float gini =
+            ((float) subProblemLeft->instanceCount / (float) sp->instanceCount) * scoreLeft
+            +
+            ((float) subProblemRight->instanceCount / (float) sp->instanceCount) * scoreRight;
+
+
+    Subproblem_destroy(subProblemLeft);
+    Subproblem_destroy(subProblemRight);
+
+    return gini;
+}
+
+float Get_gini_score(Subproblem *sp) {
+    float score = .0f;
+
+    for (int i = 0; i < sp->classCount; ++i) {
+        score += (float) pow((float )sp->classes[i].instanceCount / (float)sp->instanceCount, 2);
+    }
+
+    return 1.0f - score;
+}
