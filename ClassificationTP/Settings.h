458c4d772303d0930a740f2d440a16e5f8af55f9
adrien <a.d44@tuta.io>
2023-12-18 22:36:53 +0100

    fix: Include SDL dynamically in function of the OS

--- ClassificationTP/Settings.h
+++ ClassificationTP/Settings.h
@@ -6,12 +6,23 @@
 // Can take : "efficient" | "performance"
 #define POWER_MODE() "performance"
 
+#ifdef _WIN32
+#  include "SDL.h"
+#  include "SDL_image.h"
+#  include "SDL_ttf.h"
+#  include "SDL_mixer.h"
+#else
+#  include <SDL2/SDL.h>
+#  include <SDL2/SDL_image.h>
+#  include <SDL2/SDL_ttf.h>
+#  include <SDL2/SDL_mixer.h>
+#endif
+
 #include <assert.h>
 #include <stdbool.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <float.h>
-#include "SDL.h"
 #include "Dataset.h"
 #include "Split.h"
 #include "DecisionTree.h"
