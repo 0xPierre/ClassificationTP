0bbdc7ce1442dc563330782fe9e76b35c81dc947
adrien <a.d44@tuta.io>
2023-12-18 22:37:43 +0100

    fix: Remove main.c from the root project and rewrite the main in the ClassificationTP

--- ClassificationTP/main.c
+++ ClassificationTP/main.c
@@ -5,5 +5,19 @@ int main(int argc, char* args[]) {
     
     RunSdl();
 
+//    double start = omp_get_wtime();
+//
+//    //test_split_compute();
+//    //test_node_tree_count();
+//    //test_train_test_evaluation();
+//    //test_Dataset_bagging();
+//    test_random_forest();
+//    /*test_memory();*/
+//    //test_dump_forest();
+//
+//
+//    double end = omp_get_wtime();
+//    printf("Work took %f seconds\n", end - start);
+
     return 0;
 }
